d22a97b7faf6e6b756acee121734a924ef10e175
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-05 16:32:50 +0200

    Add eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,24 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default withNuxt(
   // Your custom configs here
+  skipFormatting,
+  {
+    rules: {
+      'vue/html-self-closing': [
+        'error',
+        {
+          html: {
+            void: 'never',
+            normal: 'always',
+            component: 'always',
+          },
+          svg: 'always',
+          math: 'always',
+        },
+      ],
+    },
+  },
 )
