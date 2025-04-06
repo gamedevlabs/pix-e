8bcd9c5b139fe32a633ba5960b19bf3db647db5f
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-06 17:34:00 +0200

    Move ColorModeSwitch

--- app.vue
+++ app.vue
@@ -31,9 +31,10 @@ const items = ref<NavigationMenuItem[]>([
   <UApp>
     <div class="min-h-screen flex flex-col">
       <!-- Topbar -->
-      <header class="h-16 border-b border-gray-200 px-6 flex items-center justify-between">
+      <header
+        class="h-16 border-b border-gray-200 dark:border-gray-800 px-6 flex items-center justify-between"
+      >
         <h1 class="text-xl font-semibold">PIX:E</h1>
-        <ColorModeSwitch />
         <div>
           <!-- Put user info, settings, logout etc. here -->
           <UAvatar src="https://i.pravatar.cc/40" alt="User" />
@@ -43,8 +44,13 @@ const items = ref<NavigationMenuItem[]>([
       <!-- Main Content Area -->
       <div class="flex flex-1 overflow-hidden">
         <!-- Sidebar -->
-        <aside class="w-64 border-r border-gray-200 p-4 overflow-y-auto">
+        <aside
+          class="w-64 border-r border-gray-200 dark:border-gray-800 p-4 overflow-y-auto flex flex-col items-stretch"
+        >
           <UNavigationMenu orientation="vertical" :items="items" />
+          <div class="mt-auto flex justify-center">
+            <ColorModeSwitch />
+          </div>
         </aside>
 
         <!-- Page Content -->
