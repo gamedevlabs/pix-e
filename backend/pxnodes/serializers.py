6c9311fe6436662b0c4c3d941d805bfeff9fe558
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-27 00:04:33 +0200

    Fix linting and formatting in backend

--- backend/pxnodes/serializers.py
+++ backend/pxnodes/serializers.py
@@ -1,8 +1,10 @@
 from rest_framework import serializers
+
 from .models import PxNode
+
 
 class PxNodeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PxNode
         fields = "__all__"
-        read_only_fields = ['owner', 'created_at', 'updated_at']
+        read_only_fields = ["owner", "created_at", "updated_at"]
--- backend/pxnodes/urls.py
+++ backend/pxnodes/urls.py
@@ -1,10 +1,11 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from .views import PxNodeViewSet
 
 router = DefaultRouter()
-router.register(r'pxnodes', PxNodeViewSet, basename='pxnode')
+router.register(r"pxnodes", PxNodeViewSet, basename="pxnode")
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
 ]
