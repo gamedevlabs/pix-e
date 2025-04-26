1c28b09349fb6554172b329879f88539a0cafbe5
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-26 17:17:28 +0200

    Check black

--- backend/llm/views.py
+++ backend/llm/views.py
@@ -1,5 +1,5 @@
 # from django.shortcuts import render
-from rest_framework.views import APIView
-from django.views import View
+#from django.views import View
+#from rest_framework.views import APIView
 
 # Create your views here.
