fccd854cf73509630df52fcf46293bc56cc06515
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-26 23:21:29 +0200

    Improve styling and code structure

---
+++ frontend/components/PxNodeCard.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+const props = defineProps<{ node: PxNode }>()
+
+function startEdit() {
+  console.log('Edit node')
+}
+
+function deletePxNode() {
+  console.log('Delete node')
+}
+</script>
+
+<template>
+  <UCard class="hover:shadow-lg transition">
+    <template #header>
+      <h2 class="font-semibold text-lg">{{ props.node.name }}</h2>
+    </template>
+    <p>{{ props.node.description }}</p>
+    <template #footer>
+      <div class="flex justify-end gap-2">
+        <UButton color="secondary" variant="soft" @click="startEdit">Edit</UButton>
+        <UButton color="error" variant="soft" @click="deletePxNode()">Delete</UButton>
+      </div>
+    </template>
+  </UCard>
+</template>
+
+<style scoped></style>
--- frontend/pages/pxnodes/[id].vue
+++ frontend/pages/pxnodes/[id].vue
@@ -9,12 +9,12 @@ const { data: node, status, error } = await useFetch<PxNode>(`http://localhost:8
   <div class="p-8">
     <div v-if="status==='pending'">Loading...</div>
     <div v-else-if="error">Error loading node.</div>
-    <div v-else>
-      <h1 class="text-2xl font-bold mb-4">{{ node?.name }}</h1>
-      <p class="text-gray-600 mb-6">{{ node?.description }}</p>
-      <NuxtLink to="/pxnodes" class="text-blue-500 underline">← Back to Nodes</NuxtLink>
+    <div v-else-if="node">
+      <PxNodeCard v-if="node" :node="node" />
+      <UButton to="/pxnodes" class="my-4">← Back to Nodes</UButton>
     </div>
   </div>
+
 </template>
 
 <style scoped>
