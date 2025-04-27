aeb98511c549988c447d8887a483630fc0fc54b1
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-04-27 16:32:57 +0200

    Add components and component definitions to backend

--- backend/pxnodes/serializers.py
+++ backend/pxnodes/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from .models import PxNode
+from .models import PxComponent, PxComponentDefinition, PxNode
 
 
 class PxNodeSerializer(serializers.ModelSerializer):
@@ -8,3 +8,15 @@ class PxNodeSerializer(serializers.ModelSerializer):
         model = PxNode
         fields = "__all__"
         read_only_fields = ["owner", "created_at", "updated_at"]
+
+
+class PxComponentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PxComponent
+        fields = "__all__"
+
+
+class PxComponentDefinitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PxComponentDefinition
+        fields = "__all__"
