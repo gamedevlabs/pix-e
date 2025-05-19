3f0afbcbbfa075746dc507149970a9fbfb50249a
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-05-19 16:08:55 +0200

    Reformat with black

--- backend/pxnodes/urls.py
+++ backend/pxnodes/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (PxComponentDefinitionViewSet, PxComponentViewSet,
-                    PxNodeViewSet)
+from .views import PxComponentDefinitionViewSet, PxComponentViewSet, PxNodeViewSet
 
 router = DefaultRouter()
 router.register(r"pxnodes", PxNodeViewSet, basename="pxnode")
--- backend/pxnodes/views.py
+++ backend/pxnodes/views.py
@@ -2,8 +2,11 @@ from rest_framework import viewsets
 
 # from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from .models import PxComponent, PxComponentDefinition, PxNode
-from .serializers import (PxComponentDefinitionSerializer,
-                          PxComponentSerializer, PxNodeSerializer)
+from .serializers import (
+    PxComponentDefinitionSerializer,
+    PxComponentSerializer,
+    PxNodeSerializer,
+)
 
 
 class PxNodeViewSet(viewsets.ModelViewSet):
