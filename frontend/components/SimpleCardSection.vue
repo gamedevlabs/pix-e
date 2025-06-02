b0c58cbe96233dc62b4056b1d70b11aef1fb8505
Julian Geheeb <julian.geheeb+github@gmail.com>
2025-06-02 15:06:13 +0200

    Apply auto spacing for children of SimpleCardSection

--- frontend/components/SimpleCardSection.vue
+++ frontend/components/SimpleCardSection.vue
@@ -5,9 +5,7 @@ const emit = defineEmits<{ (e: 'addClicked'): void }>()
 </script>
 
 <template>
-  <div
-    class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-6"
-  >
+  <div class="grid gap-6" style="grid-template-columns: repeat(auto-fill, minmax(288px, 1fr))">
     <slot />
     <div>
       <UButton
