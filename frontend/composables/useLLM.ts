ad973f54a017d42803c262e600f653dc090a3d70
SpyPlayer09 <93677345+SpyPlayer09@users.noreply.github.com>
2025-06-12 16:16:34 +0200

    Add Validate and Fix functionality to Pillars Add OpenAI Models

---
+++ frontend/composables/useLLM.ts
@@ -0,0 +1,31 @@
+﻿import type { SelectItem } from '@nuxt/ui'
+import { defineStore } from 'pinia'
+
+export const useLLM = defineStore('llm', () => {
+  const llm_models = ref([
+    {
+      label: 'Google Gemini',
+      value: 'gemini',
+      icon: 'i-simple-icons-googlegemini',
+    },
+    {
+      label: 'ChatGPT (GPT-4)',
+      value: 'openai',
+      icon: 'i-simple-icons-openai',
+    },
+    {
+      label: 'Unknown Local Model',
+      value: 'local1',
+      icon: 'i-lucide-circle-help',
+    },
+  ] satisfies SelectItem[])
+  const active_llm = ref(llm_models.value[0]?.value)
+  const llm_icon = computed(
+    () => llm_models.value.find((item) => item.value === active_llm.value)?.icon,
+  )
+  return {
+    llm_models,
+    active_llm,
+    llm_icon,
+  }
+})
