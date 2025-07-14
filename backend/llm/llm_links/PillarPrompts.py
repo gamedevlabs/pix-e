b70db8510dcf652ea93dd27c2f3a970998630ee7
SpyPlayer09 <ge78tek@mytum.de>
2025-07-14 13:07:17 +0200

    Improve Backend Structure

--- backend/llm/llm_links/PillarPrompts.py
+++ backend/llm/llm_links/PillarPrompts.py
@@ -17,3 +17,14 @@ OverallFeedbackPrompt = """Rate the following game design description
     Pillars: %s
     Do not use any markdown in your answer. Answer directly as if you are giving your
     feedback to the designer."""
+
+
+ImprovePillarPrompt = """Improve the following Game Design Pillar.
+            Check for structural issues regarding the following points:
+            1. The title does not match the description.
+            2. The intent of the pillar is not clear.
+            3. The pillar focuses on more than one aspect.
+            4. The description uses bullet points or lists.
+            Pillar Title: %s
+            Pillar Description: %s
+            Rewrite erroneous parts of the pillar and return a new pillar object."""
