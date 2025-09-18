22718c543494e81a14bb3196c0412c9c06401bea
smaddin <martin.schacherbauer@tum.de>
2025-09-18 17:54:56 +0200

    Add import/export of data functionality to px

---
+++ backend/pximportexport/urls.py
@@ -0,0 +1,8 @@
+﻿from django.urls import path
+
+from .views import ExportDataView, ImportDataView
+
+urlpatterns = [
+    path("pxexport/", ExportDataView.as_view(), name="pxexports-pxdata"),
+    path("pximport/", ImportDataView.as_view(), name="pximports-pxdata"),
+]
