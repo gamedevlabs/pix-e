7aa055233da40ab4f71a2516c83824ce3696c24d
elhould <elh.elmoust@gmail.com>
2025-10-15 18:18:27 +0200

    Add operation handler framework

--- llm_orchestrator/core/__init__.py
+++ llm_orchestrator/core/__init__.py
@@ -2,5 +2,21 @@
 Core orchestration components.
 """
 
-__all__ = []
+from llm_orchestrator.core.operation_handler import BaseOperationHandler
+from llm_orchestrator.core.handler_registry import (
+    HandlerRegistry,
+    register_handler,
+    get_handler,
+    list_operations,
+    get_registry,
+)
+
+__all__ = [
+    "BaseOperationHandler",
+    "HandlerRegistry",
+    "register_handler",
+    "get_handler",
+    "list_operations",
+    "get_registry",
+]
 
