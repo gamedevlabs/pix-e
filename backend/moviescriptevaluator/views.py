d483aa401c722ee48fbb6a05af19f9febe979c99
Can Bilgin <canbilgin.f@gmail.com>
2025-10-31 15:32:51 +0100

    set up post call correctly in backend

--- backend/moviescriptevaluator/views.py
+++ backend/moviescriptevaluator/views.py
@@ -1,6 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
-from rest_framework import status
 
 from moviescriptevaluator.models import AssetMetaData
 from moviescriptevaluator.serializers import UnrealEngineDataSerializer
@@ -10,14 +9,18 @@ class MovieScriptAssets(viewsets.ModelViewSet):
     serializer_class = UnrealEngineDataSerializer
 
     def get_queryset(self):
-        data = AssetMetaData.objects.order_by("created_at")
+        data = AssetMetaData.objects.filter(owner=self.request.user).order_by(
+            "created_at"
+        )
         return data
 
-    def post(self):
-        serializer = UnrealEngineDataSerializer(data=self.request.data, many=True)
-        if serializer.is_valid():
-            print(serializer.data)
-            serializer.save()
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data, many=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def perform_create(self, serializer):
+        # Called after validation, before response
+        print(serializer.validated_data)
+        serializer.save()
