82c0dad9d9d24e2a457a943e842c8207323c585e
Can Bilgin <canbilgin.f@gmail.com>
2025-10-31 15:36:54 +0100

    Add migration

--- backend/moviescriptevaluator/models.py
+++ backend/moviescriptevaluator/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+
 class AssetMetaData(models.Model):
-    #owner = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="assets_metadata")
-    #project_name = models.CharField(max_length=255)
+    owner = models.ForeignKey(
+        "auth.User", on_delete=models.CASCADE, related_name="assets_metadata"
+    )
+    project_name = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     class_name = models.CharField(max_length=255)
     path = models.CharField(max_length=255)
