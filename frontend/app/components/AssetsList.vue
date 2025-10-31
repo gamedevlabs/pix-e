9c9eb0f2eda4e0e51969f6966a457db7b9ea775d
Can Bilgin <canbilgin.f@gmail.com>
2025-10-31 15:35:33 +0100

    adjust and standardize the frontend

--- frontend/app/components/AssetsList.vue
+++ frontend/app/components/AssetsList.vue
@@ -1,20 +1,29 @@
 <script setup lang="ts">
+import type { Asset } from '~/utils/movie-script-evaluator'
+import type { ColumnDef } from '@tanstack/vue-table'
 
-const props = defineProps({
-    assets: {
-        type: Array,
-        default: () => []
-    },
-    loading: {
-        type: Boolean,
-        default: false,
-    },
-}); 
+const props = defineProps<{
+  assets: Asset[]
+  loading?: boolean
+}>()
 
-
+const columns: ColumnDef<Asset>[] = [
+  {
+    accessorKey: 'name',
+    header: 'Asset Name',
+  },
+  {
+    accessorKey: 'class_name',
+    header: 'UClass Name',
+  },
+  {
+    accessorKey: 'path',
+    header: 'Asset Path',
+  },
+]
 </script>
 
 <template>
-    <UTable :data="assets" :loading="loading" class="flex-1" />
+  <h4>Total Number of Assets: {{ props.assets.length }}</h4>
+  <UTable sticky :data="props.assets" :loading="props.loading" :columns="columns" class="flex-1" />
 </template>
-
--- frontend/app/pages/movie-script-evaluator/index.vue
+++ frontend/app/pages/movie-script-evaluator/index.vue
@@ -1,37 +1,40 @@
 <script setup lang="ts">
 definePageMeta({
   middleware: 'authentication',
-});
-
-const { items, loading, error, fetchAll } = useMovieScriptEvaluator();
-
-onMounted(() => {
-  fetchAll();
 })
 
+const { items, fetchAll } = useMovieScriptEvaluator()
+const { user } = useAuthentication()
+
+onMounted(() => {
+  fetchAll()
+})
 </script>
 
 <template>
-    <div class="p-8">
+  <div class="p-8">
     <h1 class="text-2xl font-bold mb-6">Movie Script Evaluator</h1>
+    <div class="mb-4">
+      <p>Welcome, {{ user?.username }}!</p>
+      <p>Your user id is {{ user?.id }}</p>
+    </div>
 
-    <div class="movie-script-evaluator-container mb-8" style="display:flex; flex-direction: row; ">
-        <div class="mb-4">
-            <h2 class="text-xl font-semibold mb-2">Uploaded Movie Script</h2>
+    <div class="movie-script-evaluator-container mb-8" style="display: flex; flex-direction: row">
+      <div class="mb-4">
+        <h2 class="text-xl font-semibold mb-2">Uploaded Movie Script</h2>
+      </div>
+      <div class="mb-4">
+        <h3>List of the Existing Assets</h3>
+        <div style="max-height: 65vh; overflow-x: scroll; overflow-y: auto; white-space: nowrap">
+          <div style="display: inline-block; vertical-align: top">
+            <AssetsList :assets="items" />
+          </div>
         </div>
-        <div class="mb-4">
-            <h3>List of the Existing Assets</h3>
-            <div style="max-height: 80vh; overflow-x: scroll; overflow-y: auto; white-space: nowrap;">
-            <div style="display:inline-block; vertical-align: top;">
-                <AssetsList :assets="items" />
-            </div>
-        </div>
-        </div>
+      </div>
     </div>
 
     <!-- Add your movie script evaluator components and logic here -->
-     
-    </div>
+  </div>
 </template>
 
 <style scoped>
