d30f77b9ddd5ee5055e98c5f3ac2511ab661baba
Can Bilgin <canbilgin.f@gmail.com>
2025-12-07 12:10:58 +0100

    add new tables

--- backend/moviescriptevaluator/models.py
+++ backend/moviescriptevaluator/models.py
@@ -1,11 +1,35 @@
+import uuid
+
 from django.db import models
 
-
-class AssetMetaData(models.Model):
+class MovieProject(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     owner = models.ForeignKey(
         "auth.User", on_delete=models.CASCADE, related_name="assets_metadata"
     )
-    project_name = models.CharField(max_length=255)
+
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=511)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "movie project"
+        verbose_name_plural = "movie projects"
+
+        def __str__(self):
+            return self.verbose_name
+
+class MovieScript(models.Model):
+    title = models.CharField(max_length=255)
+    file = models.FileField()
+
+class AssetMetaData(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    project = models.ForeignKey(
+        MovieProject, on_delete=models.CASCADE, related_name="assets_metadata"
+    )
     name = models.CharField(max_length=255)
     class_name = models.CharField(max_length=255)
     path = models.CharField(max_length=255)
@@ -19,3 +43,15 @@ class AssetMetaData(models.Model):
 
     def __str__(self):
         return self.name
+
+class RequiredAssets(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    project = models.ForeignKey(
+        MovieProject, on_delete=models.CASCADE, related_name="required_assets"
+    )
+    name = models.CharField(max_length=255)
+    purpose = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
