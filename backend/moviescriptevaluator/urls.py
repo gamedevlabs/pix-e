41619da758600d7ac9482a67267e195f7c686ad9
Can Bilgin <canbilgin.f@gmail.com>
2025-12-07 12:12:26 +0100

    introduce project structure into movie script evaluator

--- backend/moviescriptevaluator/urls.py
+++ backend/moviescriptevaluator/urls.py
@@ -1,12 +1,13 @@
 from django.urls import include, path
 from rest_framework_nested import routers
 
-from moviescriptevaluator.views import MovieScriptAssets
+from moviescriptevaluator.views import MovieScriptAssets, MovieProjectView
 
 app_name = "moviescriptevaluator"
 
 router = routers.SimpleRouter()
-router.register(r"", MovieScriptAssets, basename="movie-script-evaluator")
+router.register(r"", MovieProjectView, basename="movie-script-evaluator")
+router.register(r"/assets", MovieScriptAssets, basename="asset-metadata")
 
 urlpatterns = [
     path("", include(router.urls)),
---
+++ frontend/app/pages/movie-script-evaluator/[id].vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import type { FormSubmitEvent } from '@nuxt/ui'
+import SingleFileUpload from '~/components/SingleFileUpload.vue'
+
+definePageMeta({
+  middleware: 'authentication',
+})
+
+const { items, fetchAll } = useMovieScriptEvaluator()
+const { user } = useAuthentication()
+
+onMounted(() => {
+  fetchAll()
+})
+
+
+const state = reactive<Partial<any>>({
+  movieScriptFile: null,
+})
+
+async function onSubmit(event: FormSubmitEvent<any>) {
+  console.log(event.data);
+}
+</script>
+
+<template>
+  <div class="p-8">
+    <h1 class="text-2xl font-bold mb-6">Movie Script Evaluator</h1>
+    <div class="mb-4">
+      <p>Welcome, {{ user?.username }}!</p>
+      <p>Your user id is {{ user?.id }}</p>
+    </div>
+
+    <div class="movie-script-evaluator-container mb-8" style="display: flex; flex-direction: row">
+      <div class="mb-4">
+        <h2 class="text-xl font-semibold mb-2">Uploaded Movie Script</h2>
+        <SingleFileUpload
+          :maxFileSize="2 * 1024 * 1024" 
+          :acceptedFileTypes="['application/pdf', 'text/plain']"
+          @submit="onSubmit"
+        />
+      </div>
+      <div class="mb-4">
+        <h3>List of the Existing Assets</h3>
+        <div style="max-height: 65vh; overflow-x: scroll; overflow-y: auto; white-space: nowrap">
+          <div style="display: inline-block; vertical-align: top">
+            <AssetsList :assets="items" />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Add your movie script evaluator components and logic here -->
+  </div>
+</template>
+
+<style scoped>
+.mb-4 {
+  width: 50%;
+  max-height: 50%;
+}
+</style>
