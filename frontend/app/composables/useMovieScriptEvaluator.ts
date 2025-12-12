bcdfa52351de3b7002550bef9e17525a025151a3
Can Bilgin <c.bilgin@hff-muc.de>
2025-12-12 16:15:45 +0100

    adjust routes in backend, update frontend accordingly

--- frontend/app/composables/useMovieScriptEvaluator.ts
+++ frontend/app/composables/useMovieScriptEvaluator.ts
@@ -1,5 +1,9 @@
-import type { Asset } from '../utils/movie-script-evaluator.d.ts'
+import type { Asset, MovieProject } from '../utils/movie-script-evaluator.d.ts'
 
 export function useMovieScriptEvaluator() {
-  return useCrudWithAuthentication<Asset>('movie-script-evaluator')
+  return useCrudWithAuthentication<MovieProject>('movie-script-evaluator/')
+}
+
+export function useMovieScriptEvaluatorAssets(project_id: string) {
+  return useCrudWithAuthentication<Asset>("movie-script-evaluator/"+project_id +"/assets/")
 }
