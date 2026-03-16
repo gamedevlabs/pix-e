2b3dd3db462fcda829dbdae46766b3df31c25ea8
Rai Trouvain <raitrouvain@gmail.com>
2026-03-16 12:26:24 +0100

    add locks and keys to frontend types

---
+++ frontend/app/utils/locks-keys.d.ts
@@ -0,0 +1,46 @@
+const pxLockUnlockModes = ['permanent', 'temporary', 'reversible', 'collapsible'] as const
+type PxLockUnlockModeType = (typeof pxLockUnlockModes)[number] // TODO: improve name
+
+const pxKeyTypes = ['item', 'ability', 'none'] as const
+type PxKeyTypesType = (typeof pxKeyTypes)[number] // TODO: improve name
+
+interface PxLockDefinition {
+  id: string
+  name: string
+  unlocked_by: string[] // ids of key definitions
+  unlock_mode: PxLockUnlockModeType
+  created_at: string
+  updated_at: string
+  owner: number | null
+}
+
+interface PxKeyDefinition {
+  id: string
+  name: string
+  type: PxKeyTypesType
+  consumable: boolean
+  fixed: boolean
+  unique: boolean // can also be verified. alternatively: show summary with count of each key
+  created_at: string
+  updated_at: string
+  owner: number | null
+}
+
+interface PxLock {
+  id: string
+  px_chart: string
+  definition: string
+  edge: string
+  created_at: string
+  updated_at: string
+  owner: number | null
+}
+
+interface PxKey {
+  id: string
+  definition: string
+  node: string
+  created_at: string
+  updated_at: string
+  owner: number | null
+}
