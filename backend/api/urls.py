b2e5341bd10273a9571babff403c891abccb9ddc
Lucia <121578043+LuciaVi@users.noreply.github.com>
2026-06-14 19:20:46 +0200

    fixed pipeline issues

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import include, path
 
-urlpatterns = [
+urlpatterns: list = [
     path("admin/", admin.site.urls),
     path("api/llm/", include("pillars.urls")),
     path("api/llm/", include("pxnodes.llm_urls")),
