dc3d06c7310a2db5474b602ac0cc0ca7d7cd87dc
Rai Trouvain <raitrouvain@gmail.com>
2026-08-08 16:21:51 +0200

    show l/k symbols in cards, previews, assignment creation forms

--- frontend/app/components/PxComponents/PxLockKeyComponents/PxKeyDefinitionCardPreview.vue
+++ frontend/app/components/PxComponents/PxLockKeyComponents/PxKeyDefinitionCardPreview.vue
@@ -3,7 +3,7 @@ const props = defineProps<{ definition: PxKeyDefinition }>()
 </script>
 <template>
   <UButton :to="{ name: 'pxkeydefinitions-id', params: { id: props.definition.id } }">
-    {{ props.definition.name }}</UButton
+    {{ props.definition.symbol }} {{ props.definition.name }}</UButton
   >
 </template>
 <style scoped></style>
--- frontend/app/components/PxComponents/PxLockKeyComponents/PxLockDefinitionCardPreview.vue
+++ frontend/app/components/PxComponents/PxLockKeyComponents/PxLockDefinitionCardPreview.vue
@@ -3,7 +3,7 @@ const props = defineProps<{ definition: PxLockDefinition }>()
 </script>
 <template>
   <UButton :to="{ name: 'pxlockdefinitions-id', params: { id: props.definition.id } }">
-    {{ props.definition.name }}</UButton
+    {{ props.definition.symbol }} {{ props.definition.name }}</UButton
   >
 </template>
 <style scoped></style>
